f6bd261fd4c1c7b755bb54d535f7f2032363117f
Selim Ajimi <sloumma.21@gmail.com>
2018-04-10 15:09:12 +0200

    StripeClient charging customer

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import com.stripe.exception.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            chargeCustomer("tok_mastercard", new StripeClient());
+        } catch (CardException | APIException | InvalidRequestException | AuthenticationException | APIConnectionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void chargeCustomer(String token, StripeClient stripeClient) throws CardException, APIException, AuthenticationException, InvalidRequestException, APIConnectionException {
+        User user = new User();
+        user.setId(1);
+        if (user.getStripeCustomerId() == null || user.getStripeCustomerId().isEmpty()) {
+            stripeClient.createCustomer(user, token);
+        } else {
+            stripeClient.updateCustomerCard(user, token);
+        }
+
+        stripeClient.createInvoiceItem(
+                5 * 100,
+                user,
+                "Your Description");
+
+        // TODO: 4/9/2018
+
+
+        stripeClient.createInvoice(user, true);
+
+    }
+}
